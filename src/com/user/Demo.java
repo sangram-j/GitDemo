926836b36b9defbb6d5aecb0beceb496a9b607e7
sangram-j <sangramjadhav179@gmail.com>
2022-11-15 22:12:37 +0530

    adding variable e

--- src/com/user/Demo.java
+++ src/com/user/Demo.java
@@ -2,11 +2,12 @@ package com.user;
 
 public class Demo {
 
-	public int getData(int a,int b,int c) {
+	public int getData(int a,int b,int c, int d) {
 		a=10;
 		b=10;
 		c=10;
-		d=10;
-		return a*b*c;
+		d=a*b*c;
+		int e= d/a;
+		return e;
 	}
 }
